159b69301b52498000ce536670a30e54562710f2
Lunaris-Project <alasamouly@gmail.com>
2025-04-10 21:34:08 +0200

    added english numbers to workspaces module

--- .config/ags/modules/bar/normal/workspaces_hyprland.js
+++ .config/ags/modules/bar/normal/workspaces_hyprland.js
@@ -39,6 +39,7 @@ const getFontWeightName = (weight) => {
 // Number style conversion functions
 const numberStyles = {
     'arabic': (n) => n.toString().replace(/[0-9]/g, d => '٠١٢٣٤٥٦٧٨٩'[d]),
+    'english': (n) => n.toString(),
     'thai': (n) => n.toString().replace(/[0-9]/g, d => '๐๑๒๓๔๕๖๗๘๙'[d]),
     'japanese': (n) => n.toString().replace(/[0-9]/g, d => '〇一二三四五六七八九'[d]),
     'chinese': (n) => n.toString().replace(/[0-9]/g, d => '零一二三四五六七八九'[d]),
