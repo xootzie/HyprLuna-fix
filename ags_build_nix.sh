fb49ac039654e2c57939498a02b1c233b04a425a
Nurlyx <nurlyx@proton.me>
2025-08-08 00:32:06 +0530

    lol

--- ags_build_nix.sh
+++ ags_build_nix.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+echo "We'll be installing nix on your setup you'd have to be engaged in the process if you choose yes (y/N)"
+read -r answer
+answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+if [[ "$answer" == "no" || "$answer" == "n" || -z "$answer" ]]; then
+    echo "Bye"
+    exit
+elif [[ "$answer" != "yes" && "$answer" != "y" ]]; then
+    echo "I don't understand."
+    exit
+fi
+
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+. ~/.nix-profile/etc/profile.d/nix.sh
+export NIX_CONFIG="experimental-features = nix-command flakes"
+nix profile add github:nurly3/agsv1
