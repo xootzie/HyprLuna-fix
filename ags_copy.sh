cd449384b61ee78249bb825eda9977b96994beec
Nurlyx <nurlyx@proton.me>
2025-08-08 16:43:14 +0530

    fix: sudo

--- ags_copy.sh
+++ ags_copy.sh
@@ -2,7 +2,7 @@
 
 #!/bin/bash
 
-echo "We'll copy binaries and libraries from the repo to /usr do you wish to continue? (y/N)"
+echo "We'll copy binaries and libraries from the repo to /usr and will require sudo perms do you wish to continue? (y/N)"
 read -r answer
 answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
 if [[ "$answer" == "no" || "$answer" == "n" || -z "$answer" ]]; then
@@ -13,6 +13,6 @@ elif [[ "$answer" != "yes" && "$answer" != "y" ]]; then
     exit
 fi
 
-cp -r ./ags_bin_lib/com.github.Aylur.ags/ /usr/share/
-cp -r ./ags_bin_lib/agsv1/ /usr/lib/
-ln -sf /usr/share/com.github.Aylur.ags/com.github.Aylur.ags /usr/bin/agsv1
+sudo cp -r ./ags_bin_lib/com.github.Aylur.ags/ /usr/share/
+sudo cp -r ./ags_bin_lib/agsv1/ /usr/lib/
+sudo ln -sf /usr/share/com.github.Aylur.ags/com.github.Aylur.ags /usr/bin/agsv1
